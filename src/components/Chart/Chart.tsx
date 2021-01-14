4a7eec18ec3c14091e9fd4ae44decbb95f462923
HBTghost <hacbangtam@gmail.com>
2021-01-15 01:04:38 +0700

    feat: better chart

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import { AppContext } from "../../App";
+import { isMobile } from "react-device-detect";
 
 import styles from "./Chart.module.css";
 
@@ -20,33 +21,75 @@ const Chart = () => {
     <div className={styles.container}>
       <Line
         data={{
-          labels: chart.map(({ date }: dailyDataType) =>
-            new Date(date).toLocaleDateString()
-          ),
+          labels: chart.map(({ date }) => date),
           datasets: [
             {
-              data: chart.map(({ confirmed }: dailyDataType) => confirmed),
-              label: "CA NHIỄM",
-              borderColor: "rgba(0, 0, 255, 0.5)",
-              fill: true,
+              label: "Confirmed",
+              data: chart.map(({ confirmed }) => confirmed),
+              borderColor: "#4285f4",
+              fill: false,
+              pointRadius: isMobile ? 1 : 3,
             },
             {
-              data: chart.map(({ deaths }: dailyDataType) => deaths),
-              label: "TỬ VONG",
-              borderColor: "rgba(255, 0, 0, 0.5)",
-              fill: true,
+              label: "Recovered",
+              data: chart.map(({ recovered }) => recovered),
+              borderColor: "#0c9d58",
+              fill: false,
+              pointRadius: isMobile ? 1 : 3,
+            },
+            {
+              label: "Deaths",
+              data: chart.map(({ deaths }) => deaths),
+              borderColor: "#db4337",
+              fill: false,
+              pointRadius: isMobile ? 1 : 3,
+            },
+            {
+              label: "Active",
+              data: chart.map(
+                ({ confirmed, recovered, deaths }) =>
+                  confirmed - recovered - deaths
+              ),
+              borderColor: "#f4b400",
+              fill: false,
+              pointRadius: isMobile ? 1 : 3,
             },
           ],
         }}
         options={{
-          legend: { display: false },
+          animation: {
+            duration: 1000,
+            easing: "easeInOutQuint",
+          },
+          layout: {
+            padding: {
+              left: isMobile ? 5 : 50,
+              right: isMobile ? 5 : 50,
+              top: isMobile ? 5 : 50,
+              bottom: 10,
+            },
+          },
+          legend: {
+            labels: {
+              boxWidth: isMobile ? 10 : 40,
+              padding: 10,
+            },
+          },
           title: {
             display: true,
-            text: `Biểu đồ ${
-              state?.where !== "Global"
-                ? "của nước " + state?.where
-                : "trên thế giới"
+            text: `Biểu đồ của ${
+              state?.where !== "Global" ? state?.where : "thế giới"
             }`,
+          },
+          scales: {
+            xAxes: [
+              {
+                ticks: {
+                  maxTicksLimit: isMobile ? 4 : 10,
+                  maxRotation: 0,
+                },
+              },
+            ],
           },
         }}
       />
